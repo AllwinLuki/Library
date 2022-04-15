890363c3356aaa912eb1a1ee4a11d321b60104be
Allwin <allwinluki28@gmail.com>
2022-04-15 14:17:09 +0700

    added a function to add book to table

--- library.js
+++ library.js
@@ -1,21 +1,38 @@
 let myLibrary = [];
+const book1 = new Book('One Piece', 'Eiichiro Oda', 'Fantasy Adventure', 'Read');
+const book2 = new Book('Dunno', 'Anno Nimmus', 'Mystery', 'Not Read');
+
+var ind = myLibrary.length;
+console.log(ind);
+
+var closeButt = document.querySelector(".close");
+closeButt.addEventListener('click', () => {
+    document.querySelector(".form").style.display="none";
+})
 
 const addBookButton = document.querySelector(".addBook");
 addBookButton.addEventListener('click', openForm);
 
 const add = document.querySelector(".add");
-add.addEventListener('click', closeForm);
+add.addEventListener('click', addBookToTable);
 
 function openForm(){
    let f = document.querySelector(".form");
    f.style.display = "block";
 }
 
-function closeForm(){
-    console.log("what");
+function addBookToTable(){
 let title = document.querySelector("#title").value;
 let author = document.querySelector("#author").value;
 let genre = document.querySelector("#genre").value;
+
+if(title === "" || author === "" || genre === ""){
+    alert("Please fill out all of the fields!");
+}else{
+document.querySelector('#title').value = null;
+document.querySelector('#author').value = null;
+document.querySelector('#genre').value = null;
+
 let readStatus = "";
 if(document.querySelector("#read").checked){
 readStatus = "Read";
@@ -24,9 +41,11 @@ readStatus = "Read";
 }
 addBook(new Book(title, author, genre, readStatus));
 
-console.log(myLibrary[2].author + " " + myLibrary[2].readStat);
+ind = myLibrary.length - 1;
+makeTable();
 
 document.querySelector(".form").style.display="none";
+}
 }
 
 function Book(title, author, genre, readStat){
@@ -36,14 +55,37 @@ function Book(title, author, genre, readStat){
     this.readStat = readStat;
 }
 
-const book1 = new Book('One Piece', 'Eiichiro Oda', 'Fantasy Adventure', 'Read');
-const book2 = new Book('Dunno', 'Anno Nimmus', 'Mystery', 'Not Read');
 
 function addBook(Book){
     myLibrary.push(Book);
 }
 
-
-
-addBook(book1);
-addBook(book2);
+function makeTable(){
+    let table = document.querySelector(".bookTable");
+ 
+    let i = ind;
+    ind = 0;
+    myLibrary.forEach(function(item, index){
+        if(ind >= i){
+        let row = document.createElement('tr');
+        let titl = document.createElement('td');
+        titl.appendChild(document.createTextNode(item.title));
+        row.appendChild(titl);
+        let auth = document.createElement('td');
+        auth.appendChild(document.createTextNode(item.author));
+        row.appendChild(auth);
+        let gen = document.createElement('td');
+        gen.appendChild(document.createTextNode(item.genre));
+        row.appendChild(gen);
+        let r = document.createElement('td');
+        r.appendChild(document.createTextNode(item.readStat));
+        row.appendChild(r);
+        
+        table.appendChild(row);
+        ind++;
+        }else{
+        ind++;
+        }
+    })
+    ind = myLibrary.length;
+}
